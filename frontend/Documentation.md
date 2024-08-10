7f2a3315d54c36b96996183d75d3bb5c51d3bbcc
ajedral <ajedral1994@gmail.com>
2024-08-10 14:15:35 +0800

    add documentation

--- frontend/Documentation.md
+++ frontend/Documentation.md
@@ -14,7 +14,10 @@ Visit [Mustache](https://mustache.github.io/), we'll be using this tool to creat
 | *4* | ...         |             |             |
 | **sheet_1** | sheet_2   | ...
 <br>
->***Note:*** Property name must not contain space character. Properties starts at **row 2** and indexes starts at **column B**
+
+
+> ***Note***: Property name must not contain space character. Properties starts at **row 2** and indexes starts at **column B**
+
 ---
 
 ### Structure of data extracted from Google Sheet
--- frontend/src/Pages/Documentation.tsx
+++ frontend/src/Pages/Documentation.tsx
@@ -1,7 +1,40 @@
 import { Fragment } from "react/jsx-runtime";
+import MarkdownPreview from "@uiw/react-markdown-preview";
+import { readFileDataAsBase64 } from "../Utils/FileHandlers";
+import fs from 'fs/promises'
+// import documentation from '../../Documentation.md'
+import { useEffect, useState } from "react";
+
+
 
 export default function Documentation() {
+    const wrapper_element = {
+        "data-color-mode": "light"
+    }
+
+    const [markdown, setMarkdown] = useState("")
+
+    useEffect(() => {
+        import('../../Documentation.md')
+            .then(res => {
+                fetch(res.default)
+                    .then(res => res.text())
+                    .then(res => setMarkdown(res))
+                    .catch(err => console.log(err))
+            })
+    }, [])
+
+
+
+    // fs.readFile('',).then(res => console.log(res)).catch(err => console.log(err.message))
+
+
+    // base64.then(res => console.log(res))
+
+
+
+
     return <Fragment>
-        <h1>Hello from Documentation</h1>
+        <MarkdownPreview source={markdown} wrapperElement={wrapper_element} style={{ padding: "0" }} />
     </Fragment>
 }
