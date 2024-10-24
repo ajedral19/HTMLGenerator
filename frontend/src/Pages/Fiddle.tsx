00b36dfa12e38eb7a9c0ce2b3a7682b7d8cf92ab
ajedral <ajedral1994@gmail.com>
2024-10-25 00:18:24 +0800

    restruturing fiddle

--- frontend/src/Pages/Fiddle.tsx
+++ frontend/src/Pages/Fiddle.tsx
@@ -1,104 +1,40 @@
 import AceEditor from 'react-ace'
 import "ace-builds/src-noconflict/mode-html";
-import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-dracula";
 import "ace-builds/src-noconflict/ext-language_tools";
-import { useEffect, useState } from 'react';
-import DOMPurify from 'dompurify'
-import MarkdownPreview from '@uiw/react-markdown-preview';
-import { useSelector } from 'react-redux';
-import useExtract from '../Hooks/useExtract';
-import Handlebars, { registerHelper } from 'handlebars'
+import style from '../Styles/fiddle.module.sass'
 import cn from 'classnames'
-import style from '../Styles/global.module.sass'
-import { Button } from '../Components/Widgets';
-import Field from '../Components/Widgets/field.widget';
+import { FaChevronDown } from 'react-icons/fa6';
+import { IoCodeDownloadOutline } from 'react-icons/io5';
+import { RiFileUploadFill } from 'react-icons/ri';
+
 
 export default function Fiddle() {
-    const [html, setHtml] = useState<string>("<h1>hello</h1>")
-    const [htmlOutput, setHtmlOutput] = useState<string>("")
-    const { url } = useSelector((state: { spreadsheet: { url: string } }) => state.spreadsheet)
-    const { data, isLoading } = useExtract(url)
 
-    useEffect(() => {
-        Handlebars.registerHelper("loud", (text: string) => text.toUpperCase())
-        Handlebars.registerHelper("testhelper", (text: string, phrase: string) => {
-            return [
-                text.toUpperCase(),
-                phrase.toLowerCase()
-            ]
-        })
-        Handlebars.registerHelper("blank", (text: string, ...args) => {
-            const replace = (str: string) => (str.split('').map(_ => '_').join(''))
-            console.log(args);
-            return args.map(arg => {
-                if (typeof arg !== 'object')
-                    return (text.replace(arg, replace(arg)))
-
-            })
-        })
-
-        const compile = Handlebars.compile(html)
-
-        try {
-            if (data.rows) {
-                const render = compile(data.rows[0])
-                setHtmlOutput(render)
-            } else {
-                const render = compile(data)
-                setHtmlOutput(render)
-
-            }
-        } catch (e) {
-            setHtmlOutput(html)
-        }
-    }, [html, data])
-
-    const handleOnChange = (e: string) => {
-        setHtml(e)
-    }
 
     return <>
-        <div className={cn(style.fiddle)}>
-            <div className={cn(style.fiddle__data)}>
-                <MarkdownPreview
-                    source={isLoading ? `\`\`\`json\n["loading"]` : `\`\`\`json\n${JSON.stringify(data.rows ? data.rows[0] : data, null, "  ")}`} />
-            </div>
-
-            <div className={cn(style.fiddle__editor)}>
-                <AceEditor
-                    placeholder='Play here'
-                    name='test_editor'
-                    onChange={(e) => handleOnChange(e)}
-                    value={html}
-                    width='100%'
-                    height='100%'
-                    fontSize="1.6rem"
-                    mode="html"
-                    theme="twilight"
-                    lineHeight="1.6"
-                    editorProps={{ $blockScrolling: true }}
-                    setOptions={{
-                        enableLiveAutocompletion: true,
-                        enableBasicAutocompletion: true,
-                        enableMultiselect: true,
-                        enableSnippets: true,
-                        showLineNumbers: true,
-                        enableEmmet: true,
-                        tabSize: 2,
-                        showGutter: true,
-                        showFoldWidgets: true,
-                        showPrintMargin: true,
-                        highlightActiveLine: true,
-                        displayIndentGuides: true,
-                    }}
-                />
-
-            </div>
-            <div className={cn(style.fiddle__view)}>
-                <div
-                    dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(htmlOutput, { IN_PLACE: true, FORBID_TAGS: ['style', 'script'], }) }}>
+        <section className={cn(style.fiddle)}>
+            <aside className={cn(style.fiddle_finder)}></aside>
+            <main className={cn(style.fiddle__main_panel)}>
+                <div className={cn(style.fiddle_pane, style.fiddle_editor_pane)}>
+                    <div className={cn(style.fiddle_pane__header)}>
+                        <p>Click to add title</p>
+                        <p><RiFileUploadFill /> Save</p>
+                    </div>
+                    <div className={cn(style.fiddle_pane__body, style.editor_wrap)}>
+                        <AceEditor name='fiddle_editor' height='100%' width='100%' mode="html" placeholder='Fiddle' theme='dracula' />
+                    </div>
                 </div>
-            </div>
-        </div>
+                <div className={cn(style.fiddle_pane, style.fiddle_result_pane)}>
+                    <div className={cn(style.fiddle_pane__header)}>
+                        <p>Choose Stylesheet <FaChevronDown fontSize="1.2rem" /></p>
+                        <p>Download <IoCodeDownloadOutline /></p>
+                    </div>
+                    <div className={cn(style.fiddle_pane__body, style.result_wrap)}>
+
+                    </div>
+                </div>
+            </main>
+        </section>
     </>
 }
