86d483e2526ea0f29a09c5d4d80c1d3e668d7aff
ajedral19 <ajedral1994@gmail.com>
2024-10-25 18:21:04 +0800

    minor changes

--- frontend/src/Pages/Fiddle.tsx
+++ frontend/src/Pages/Fiddle.tsx
@@ -14,12 +14,22 @@ export default function Fiddle() {
 
     return <>
         <section className={cn(style.fiddle)}>
-            <aside className={cn(style.fiddle_finder)}></aside>
+            <aside className={cn(style.fiddle_finder)}>
+                {/* ANCHOR panels */}
+                <div className={cn(style.panels)}>
+                    <ul className={cn(style.panels__tabs)}>
+                        <li role='button' className={cn(style.panels__tabs__tab)}>Data</li>
+                        <li role='button' className={cn(style.panels__tabs__tab)}>CSS</li>
+                        <li role='button' className={cn(style.panels__tabs__tab)}>Helpers</li>
+                    </ul>
+                    <div className={cn(style.panels__panel)}></div>
+                </div>
+            </aside>
             <main className={cn(style.fiddle__main_panel)}>
                 <div className={cn(style.fiddle_pane, style.fiddle_editor_pane)}>
                     <div className={cn(style.fiddle_pane__header)}>
                         <p>Click to add title</p>
-                        <p><RiFileUploadFill /> Save</p>
+                        <p role='button'><RiFileUploadFill /> Save</p>
                     </div>
                     <div className={cn(style.fiddle_pane__body, style.editor_wrap)}>
                         <AceEditor name='fiddle_editor' height='100%' width='100%' mode="html" placeholder='Fiddle' theme='dracula' />
@@ -28,7 +38,7 @@ export default function Fiddle() {
                 <div className={cn(style.fiddle_pane, style.fiddle_result_pane)}>
                     <div className={cn(style.fiddle_pane__header)}>
                         <p>Choose Stylesheet <FaChevronDown fontSize="1.2rem" /></p>
-                        <p>Download <IoCodeDownloadOutline /></p>
+                        <p role='button'>Download <IoCodeDownloadOutline /></p>
                     </div>
                     <div className={cn(style.fiddle_pane__body, style.result_wrap)}>
 
